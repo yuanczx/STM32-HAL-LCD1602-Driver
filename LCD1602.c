8e7de82cd0f3a07de45caaa1d0e6993912532c86
yuanczx <319333286@qq.com>
2021-07-20 11:05:19 +0800

    完善使用方法

--- LCD1602.c
+++ LCD1602.c
@@ -2,12 +2,12 @@
 // Created by yuanczx on 2021/7/19.
 //
 
-#include "../Inc/LCD1602.h"
-#include "gpio.h"
+#include "LCD1602.h"
 
-
-void initLCD(uint8_t position)
+GPIO_TypeDef* GPIOP;
+void initLCD(u8 position,GPIO_TypeDef* DATA_PORT)
 {
+    GPIOP=DATA_PORT;
     HAL_GPIO_WritePin(E_GPIO_Port,E_Pin,0);
     writeComd(0x38);
     writeComd(0x0c);
@@ -17,24 +17,24 @@ void initLCD(uint8_t position)
     HAL_Delay(2);
 
 }
-void writeComd(uint8_t comd)
+void writeComd(u8 comd)
 {
     HAL_GPIO_WritePin(RS_GPIO_Port,RS_Pin,0);
     HAL_GPIO_WritePin(RW_GPIO_Port,RW_Pin,0);
 
-    GPIOB->ODR=comd;
+    GPIOP->ODR=comd;
 
     HAL_GPIO_WritePin(E_GPIO_Port,E_Pin,1);
     HAL_Delay(1);
     HAL_GPIO_WritePin(E_GPIO_Port,E_Pin,0);
 }
 
-void writeData(uint8_t data)
+void writeData(u8 data)
 {
     HAL_GPIO_WritePin(RS_GPIO_Port,RS_Pin,1);
     HAL_GPIO_WritePin(RW_GPIO_Port,RW_Pin,0);
 
-    GPIOB->ODR=data;
+    GPIOP->ODR=data;
 
     HAL_GPIO_WritePin(E_GPIO_Port,E_Pin,1);
     HAL_Delay(1);
@@ -48,7 +48,7 @@ void deleteData()
     writeComd(0x10);
 }
 
-void showCursor(uint8_t stopFlash)
+void showCursor(u8 stopFlash)
 {
     //0；flash 1:stop flash
     writeComd(0x0f-stopFlash);
--- README.md
+++ README.md
@@ -3,17 +3,25 @@
 
 LCD1602 driver based on STM32 HAL library
 
+## 注意：
+- 使用前请正确配置*User Label* 如RW引脚对应*User Label*应设置成"RW"
+
 ## 基本使用：
 ``` C
 #include  "LCD1602.h" 
+...
 
-initLCD(1);
-writeData(0x41);
+void main()
+{
+	initLCD(0,GPIOX);
+	writeData(0x41);
+	while(1);
+}
 ```
 ## 方法说明：
-- ```void initLCD(uint8_t position)```		*初始化LCD position->显示位置*
-- ```void writeComd(uint8_t comd)``` 		*写指令*
-- ```void writeData(uint8_t data)```		*写数据*
+- ```void initLCD(u8 position,GPIO_TypeDef* DATA_PORT)```	*初始化LCD position->显示位置 DATA_PORT->数据端口*
+- ```void writeComd(u8 comd)``` 		*写指令*
+- ```void writeData(u8 data)```		*写数据*
 - ```void deleteData()```					*删除一位*
-- ```void showCursor(uint8_t stopFlash)```	*显示光标 stopFLash->0:光标闪烁 1:光标不闪烁*
+- ```void showCursor(u8 stopFlash)```	*显示光标 stopFLash->0:光标闪烁 1:光标不闪烁*
 - ```void closeCursor()```					*关闭光标*
